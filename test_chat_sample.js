e926330b79449b5c6c076e0ed6fcacdb0605cd56
b0bbaTV <dylan.harvey@me.com>
2025-07-10 13:56:11 +0100

    Update test_chat_sample.js

--- test_chat_sample.js
+++ test_chat_sample.js
@@ -1,13 +1,1 @@
 
-window.addEventListener('DOMContentLoaded', () => {
-  const container = document.getElementById('chat-container');
-  const sample = document.createElement('section');
-  sample.className = 'bubble animate';
-  sample.innerHTML = `
-    <div class="username-box">
-      <div class="username">b0bbaTV <span class="pronouns-badge">(TidyRoom-69)</span></div>
-    </div>
-    <div class="message"><span class="message-wrapper">omg I just won bingo again 😩🎉</span></div>
-  `;
-  container.appendChild(sample);
-});
